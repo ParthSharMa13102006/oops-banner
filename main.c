4ff7ebf5beb161a11837cf1aafe05a03d5636f38
ParthSharMa13102006 <ps141394@gmail.com>
2026-02-17 10:41:56 +0530

    UC1 commit

---
+++ main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+// {
+//     printf("ph");
+//     return 0;
+// }
+// {
+//     printf("ooooo");
+//     return 0;
+// }
+/*{
+    char name[] =" Parth Sharma";
+    printf("Hello i am %s\n",name);
+    return 0;
+}*/
+// 
+
+
+
+// #include <stdbool.h>
+// int main()
+// {
+//     bool isOnline=1 ;
+//     if(isOnline){
+//         printf("You are online");
+//     }
+//     else {
+//         printf("You are offline");
+//      }
+// }
+
+
+
+//  int main()
+// { 
+// int k=1; 
+// printf("%d == 1 is" "%s\n", k, k==1?"TRUE":"FALSE"); 
+// return 0; 
+// }
+
+int main()
+{
+    int x = 0;
+    x = printf("Hello World");
+    printf(" %d", x);
+    return 0;
+} 
